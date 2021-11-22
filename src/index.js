548f40dd5a0b32a59bb6e14707c793d6c63ae054
Noah <crashlive112@gmail.com>
2021-11-22 18:47:42 -0500

    commit tree 7e50695aeb1f727fccfe12247f97cd6551e696b1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('%c HI', 'color: firebrick')
+//On Load
+document.addEventListener('DOMContentLoaded', fetchImages);
+document.addEventListener('DOMContentLoaded', fetchBreeds);
+
+// Url's for fetching
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+// Query Selectors
+const imageContainer = document.querySelector('div#dog-image-container')
+const breedList = document.querySelector('body ul#dog-breeds')
+const listItems = document.querySelectorAll('li')
+
+//Handle Images
+function fetchImages() {
+  fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => appendImagesToContainer(json))
+}
+
+function appendImagesToContainer(json) {
+  json.message.forEach(element => {
+    let img = document.createElement('img')
+    img.src = element
+    imageContainer.appendChild(img)
+  });
+}
+
+//Handle Breeds
+function fetchBreeds() {
+  fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => appendBreedsToUL(json))
+}
+
+function appendBreedsToUL(json) {
+  Object.keys(json.message).forEach(element => {
+    let li = document.createElement('li')
+    li.innerText = element
+    breedList.appendChild(li)
+    onClickChangeColor(li)
+  })
+}
+
+function onClickChangeColor(li) {
+  li.addEventListener('click', (li) => li.target.style.color = 'red')
+}
